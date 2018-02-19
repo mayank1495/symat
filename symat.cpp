baeb6b97475b80f16e672fe7768630dc0175447d
mayank1495 <mayank007.win@gmail.com>
2018-02-20 03:50:36 +0530

    Exception handling

--- symat.cpp
+++ symat.cpp
@@ -97,6 +97,17 @@ class Symat
 SYMAT_TEMPLATE
 SYMAT_MATRIX_TYPE::Symat(const EIGEN_DYNAMIC_MATRIX_TYPE &mat)
 {
+
+	try{
+		if(!(mat.rows()==mat.cols()))
+		 throw -1;
+	}
+	catch(...)
+	{
+		cout<<"\nError: Passed Eigen::Matrix should be a generic square matrix.\n";
+		return;
+	}
+
 	s_dim = mat.rows();
 	for (int i = 0; i < s_dim; i++)
 		for (int j = 0; j < s_dim; j++)
@@ -127,15 +138,19 @@ SType SYMAT_MATRIX_TYPE::operator()(int i, int j) const
 // and stores the result in the temp Eigen::Matrix.
 // The temp matrix is resized to the dimension of the symmetric matrix.
 // NOTE: The dimension of the matrices should be same.
+// If the dimensions are different , error message is shown.
 // The result is returned as Eigen::Matrix.
 SYMAT_TEMPLATE
 EIGEN_DYNAMIC_MATRIX_TYPE SYMAT_MATRIX_TYPE::add(const EIGEN_DYNAMIC_MATRIX_TYPE &mat)
 {
-	EIGEN_DYNAMIC_MATRIX_TYPE temp;
-	temp.resize(s_dim, s_dim);
 
-	if (s_dim == mat.rows() && s_dim == mat.cols())
+	EIGEN_DYNAMIC_MATRIX_TYPE temp;
+	try
 	{
+		if (!(s_dim == mat.rows() && s_dim == mat.cols()))
+			throw - 1;
+
+		temp.resize(s_dim, s_dim);
 		for (int i = 0; i < s_dim; i++)
 		{
 			for (int j = 0; j < s_dim; j++)
@@ -143,8 +158,12 @@ EIGEN_DYNAMIC_MATRIX_TYPE SYMAT_MATRIX_TYPE::add(const EIGEN_DYNAMIC_MATRIX_TYPE
 				temp(i, j) = mat(i, j) + this->operator()(i, j);
 			}
 		}
-		return temp;
 	}
+	catch (...)
+	{
+		cout << "\nError: Dimension of matrices are different.\n";
+	}
+	return temp;
 }
 
 // Symat matrix is passed as parameter and a temporary Eigen::Matrix
@@ -156,10 +175,14 @@ EIGEN_DYNAMIC_MATRIX_TYPE SYMAT_MATRIX_TYPE::add(const EIGEN_DYNAMIC_MATRIX_TYPE
 SYMAT_TEMPLATE
 EIGEN_DYNAMIC_MATRIX_TYPE SYMAT_MATRIX_TYPE::add(const SYMAT_MATRIX_TYPE &s_mat)
 {
+
 	EIGEN_DYNAMIC_MATRIX_TYPE temp;
-	temp.resize(s_dim, s_dim);
-	if (s_dim == s_mat.dim())
+	try
 	{
+		if (!(s_dim == s_mat.dim()))
+			throw - 1;
+
+		temp.resize(s_dim, s_dim);
 		for (int i = 0; i < s_dim; i++)
 		{
 			for (int j = 0; j < s_dim; j++)
@@ -167,8 +190,12 @@ EIGEN_DYNAMIC_MATRIX_TYPE SYMAT_MATRIX_TYPE::add(const SYMAT_MATRIX_TYPE &s_mat)
 				temp(i, j) = s_mat(i, j) + this->operator()(i, j);
 			}
 		}
-		return temp;
 	}
+	catch (...)
+	{
+		cout << "\nError: Dimension of matrices are different.\n";
+	}
+	return temp;
 }
 
 // Eigen::Matrix is passed as parameter and a temporary Eigen::Matrix
@@ -182,10 +209,12 @@ SYMAT_TEMPLATE
 EIGEN_DYNAMIC_MATRIX_TYPE SYMAT_MATRIX_TYPE::sub(const EIGEN_DYNAMIC_MATRIX_TYPE &mat)
 {
 	EIGEN_DYNAMIC_MATRIX_TYPE temp;
-	temp.resize(s_dim, s_dim);
-
-	if (s_dim == mat.rows() && s_dim == mat.cols())
+	try
 	{
+		if (!(s_dim == mat.rows() && s_dim == mat.cols()))
+			throw - 1;
+
+		temp.resize(s_dim, s_dim);
 		for (int i = 0; i < s_dim; i++)
 		{
 			for (int j = 0; j < s_dim; j++)
@@ -193,8 +222,13 @@ EIGEN_DYNAMIC_MATRIX_TYPE SYMAT_MATRIX_TYPE::sub(const EIGEN_DYNAMIC_MATRIX_TYPE
 				temp(i, j) = this->operator()(i, j) - mat(i, j);
 			}
 		}
-		return temp;
 	}
+	catch (...)
+	{
+		cout << "\nError: Dimension of matrices are different.\n";
+	}
+
+	return temp;
 }
 
 // Symat Matrix is passed as parameter and a temporary Eigen::Matrix
@@ -208,10 +242,12 @@ SYMAT_TEMPLATE
 EIGEN_DYNAMIC_MATRIX_TYPE SYMAT_MATRIX_TYPE::sub(const SYMAT_MATRIX_TYPE &s_mat)
 {
 	EIGEN_DYNAMIC_MATRIX_TYPE temp;
-	temp.resize(s_dim);
-
-	if (s_dim == s_mat.dim())
+	try
 	{
+		if (!(s_dim == s_mat.dim()))
+			throw - 1;
+
+		temp.resize(s_dim);
 		for (int i = 0; i < s_dim; i++)
 		{
 			for (int j = 0; j < s_dim; j++)
@@ -219,8 +255,13 @@ EIGEN_DYNAMIC_MATRIX_TYPE SYMAT_MATRIX_TYPE::sub(const SYMAT_MATRIX_TYPE &s_mat)
 				temp(i, j) = this->operator()(i, j) - s_mat(i, j);
 			}
 		}
-		return temp;
 	}
+	catch (...)
+	{
+		cout << "\nError: Dimension of matrices are different.\n";
+	}
+
+	return temp;
 }
 
 // Eigen::Matrix is passed as parameter and a temporary Eigen::Matrix
@@ -233,13 +274,15 @@ EIGEN_DYNAMIC_MATRIX_TYPE SYMAT_MATRIX_TYPE::sub(const SYMAT_MATRIX_TYPE &s_mat)
 SYMAT_TEMPLATE
 EIGEN_DYNAMIC_MATRIX_TYPE SYMAT_MATRIX_TYPE::mul(const EIGEN_DYNAMIC_MATRIX_TYPE &mat)
 {
-	if (s_dim == mat.rows())
+	EIGEN_DYNAMIC_MATRIX_TYPE temp;
+	try
 	{
-		EIGEN_DYNAMIC_MATRIX_TYPE temp;
+		if (!(s_dim == mat.rows()))
+			throw - 1;
+
 		int t_rows = s_dim, t_cols = mat.cols();
 		temp.resize(t_rows, t_cols);
-
-		long long sum = 0;
+		SType sum = 0;
 		for (int i = 0; i < t_rows; i++)
 		{
 			for (int j = 0; j < t_cols; j++)
@@ -252,9 +295,14 @@ EIGEN_DYNAMIC_MATRIX_TYPE SYMAT_MATRIX_TYPE::mul(const EIGEN_DYNAMIC_MATRIX_TYPE
 				temp(i, j) = sum;
 			}
 		}
-
-		return temp;
 	}
+	catch (...)
+	{
+		cout << "\nError: Matrices do not qualify for Matrix multiplication. \n";
+		cout << "The number of columns of the Symat Matrix should be equal to\n"
+			 << "the number of rows of the passed Matrix.\n";
+	}
+	return temp;
 }
 
 // Symat Matrix is passed as parameter and a temporary Eigen::Matrix
@@ -267,13 +315,15 @@ EIGEN_DYNAMIC_MATRIX_TYPE SYMAT_MATRIX_TYPE::mul(const EIGEN_DYNAMIC_MATRIX_TYPE
 SYMAT_TEMPLATE
 EIGEN_DYNAMIC_MATRIX_TYPE SYMAT_MATRIX_TYPE::mul(const SYMAT_MATRIX_TYPE &s_mat)
 {
-	if (s_dim == s_mat.dim())
+	EIGEN_DYNAMIC_MATRIX_TYPE temp;
+	try
 	{
-		EIGEN_DYNAMIC_MATRIX_TYPE temp;
+		if (!(s_dim == s_mat.dim()))
+			throw - 1;
+
 		int t_rows = s_dim, t_cols = s_dim;
 		temp.resize(t_rows, t_cols);
-
-		long long sum = 0;
+		SType sum = 0;
 		for (int i = 0; i < t_rows; i++)
 		{
 			for (int j = 0; j < t_cols; j++)
@@ -286,31 +336,51 @@ EIGEN_DYNAMIC_MATRIX_TYPE SYMAT_MATRIX_TYPE::mul(const SYMAT_MATRIX_TYPE &s_mat)
 				temp(i, j) = sum;
 			}
 		}
-
-		return temp;
 	}
+	catch (...)
+	{
+		cout << "\nError: Matrices do not qualify for Matrix multiplication. \n";
+		cout << "The number of columns of the Symat Matrix should be equal to\n"
+			 << "the number of rows of the passed Matrix.\n";
+	}
+	return temp;
 }
 
 int main()
 {
-	MatrixXf m(3, 3);
-	Matrix<float, Dynamic, Dynamic> z;
-	z.resize(3, 3);
-	m << 1, 2, 3, 1, 2, 3, 1, 2, 3;
-	z << 1, 1, 1, 0, 1, 1, 1, 1, 1;
-	cout << m << endl
-		 << endl;
-	cout << z << endl
-		 << endl;
-	Symat<float> S(m);
-	Symat<float> S2(z);
-	cout << "S:\n"
-		 << S;
-	cout << "S2:\n"
-		 << S2;
-	Matrix<float, Dynamic, Dynamic> kk = S.mul(z);
-	// MatrixXf kk = S.add(S2);
-	cout << kk << endl;
-	// S2.add(m);
-	cout << "in main.." << endl;
+	MatrixXf mat3f(3,3), mat2f(2,2),iden3f(2,3);
+	mat3f<<1,2,3,1,2,3,1,2,3;
+	mat2f<<2,3,3,2;
+	iden3f<<1,2,1,2,1,2;
+	Symat<float> S(iden3f);
+	cout<<"S:"<<S;
+	// MatrixXf m(3, 3);
+	// Matrix<float, Dynamic, Dynamic> z;
+	// z.resize(3, 3);
+	// m << 1, 2, 3, 1, 2, 3, 1, 2, 3;
+	// z << 1, 1, 1, 0, 1, 1, 1, 1, 1;
+	// cout << m << endl
+	// 	 << endl;
+	// cout << z << endl
+	// 	 << endl;
+	// Symat<float> S(m);
+	// Symat<float> S2(z);
+	// cout << "S:\n"
+	// 	 << S;
+	// cout << "S2:\n"
+	// 	 << S2;
+	// Matrix<float, Dynamic, Dynamic> kk = S.mul(z);
+	// // MatrixXf kk = S.add(S2);
+	// cout << kk << endl;
+	// // S2.add(m);
+	// cout << "in main.." << endl;
+	// MatrixXf mat1(3, 3);
+	// mat1 << 1, 2, 3, 1, 2, 3, 1, 2, 3;
+	// Symat<float> SS(mat1);
+	// MatrixXf tw(2, 2);
+	// tw << 1, 2, 1, 2;
+	// MatrixXf tp = SS.mul(tw);
+	// cout << "SS: \n"
+	// 	 << tp;
+	// Symat<float> In;
 }
